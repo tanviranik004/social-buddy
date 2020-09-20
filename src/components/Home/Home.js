6b85166f0bb745e12660b2ae06a608beac9c1b9f
tanviranik004 <tanvir.anik.009@gmail.com>
2020-09-20 22:09:54 +0600

    social-buddy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,7 +24,7 @@ const classes = useStyles();
             <PeopleIcon></PeopleIcon>
              
             </IconButton>
-            <Typography variant="h6" color="inherit"> Social Buddy </Typography>
+            <Typography variant="h6" color="inherit"> Social Buddy-village-community </Typography>
             
             <Button  className={classes.logout} color="inherit">LogOut</Button>
             
